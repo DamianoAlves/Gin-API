5f9e2200c1d2aca0827656c35e886058f77233e2
Damiano <damiano.alves@gmail.com>
2022-05-30 11:16:32 -0300

    #1 Basic golang usage

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+
+	var variable1 string = "Variables need type assign"
+	var variable2 int = 1000
+	var variable3 float64 = 1000.00
+	var variable4 bool = true
+	variable5 := "String variable"
+	variable6 := 1000
+
+}
+
+func first_function() string {
+	return "Function return"
+}
